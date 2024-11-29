af5d822a4aaa81d6f36f12cf8c34eca079b6efd5
Azerus96 <gencinski1996@gmail.com>
2024-11-29 07:16:30 +0300

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,10 @@
+# gunicorn.conf.py
+import multiprocessing
+
+bind = "0.0.0.0:10000"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "uvicorn.workers.UvicornWorker"
+timeout = 120
+keepalive = 5
+max_requests = 1000
+max_requests_jitter = 50
